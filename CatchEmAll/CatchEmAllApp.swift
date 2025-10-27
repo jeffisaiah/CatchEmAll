e4fcbba9818d7447f20b86730f4766b954dd16d0
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-10-27 00:05:13 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Jeff Gutierrez on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
